5bebf18f9050983bfd027b3c8444ae8a05c98965
Jose A. Fernandez <fernandezja@gmail.com>
2014-05-27 15:28:48 -0300

    Listen implementation requests a port http specific.
    Listen implementation requests a port http specific. Only Test. Sample
    Owin (via Microsoft.Owin.Hosting and Microsoft.Owin.Host.HttpListener)

---
+++ src/OpaitemaLightWebServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpaitemaLightWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Only Test... Should set up via input parameters
+            int port = 9090; 
+            string uri = String.Format("http://localhost:{0}/", port);
+
+            using (Microsoft.Owin.Hosting.WebApp.Start<StartupForOwin>(uri))
+            {
+                Console.WriteLine("Opaitema LightWebserver on {0} is up.", uri);
+                Console.ReadKey();
+                
+            }
+
+            Console.WriteLine("Opaitema LightWebserver on {0} is close.", uri);
+            Console.WriteLine("Press any key for close.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ src/OpaitemaLightWebServer/StartupForOwin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OpaitemaLightWebServer.StartupForOwin))]
+
+namespace OpaitemaLightWebServer
+{
+    public class StartupForOwin
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.Run(context =>
+            {               
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Opaitema LightWebserver is up!");
+            });
+
+        }
+    }
+}
