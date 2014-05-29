ca51daf5cd37f9a0682af95f4c9560c04612f307
Jose A. Fernandez <fernandezja@gmail.com>
2014-05-29 14:17:19 -0300

    Set up via input parameters
    Parameter: -f: Root Folder, -p: Port, -d: Debug Request

---
+++ src/OpaitemaLightWebServer/Config.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpaitemaLightWebServer
+{
+    /// <summary>
+    /// General Configuration (Singleton)
+    /// </summary>
+    public class Config
+    {         
+        private static Config instance;
+
+        private Config() { }
+
+        public static Config Instance
+        {
+            get 
+            {
+                if (instance == null)
+                {
+                    instance = new Config();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Directory where the resources of each request
+        /// </summary>
+        public string RootDirectory { get; set; }
+
+        /// <summary>
+        /// Http port
+        /// </summary>
+        public int Port { get; set; }
+
+        public string Uri
+        {
+            get {
+            return String.Format("http://localhost:{0}", Port.ToString());
+        }
+        }
+
+        /// <summary>
+        /// View each item of the request in the query
+        /// </summary>
+        public bool DebugRequest { get; set; }
+        
+        
+    }
+}
--- src/OpaitemaLightWebServer/Program.cs
+++ src/OpaitemaLightWebServer/Program.cs
@@ -10,20 +10,64 @@ namespace OpaitemaLightWebServer
     {
         static void Main(string[] args)
         {
-            //TODO: Only Test... Should set up via input parameters
-            int port = 9090; 
-            string uri = String.Format("http://localhost:{0}/", port);
+            //Set up via input parameters
+            ConfigInitWithParameters(args);
 
-            using (Microsoft.Owin.Hosting.WebApp.Start<StartupForOwin>(uri))
+            using (Microsoft.Owin.Hosting.WebApp.Start<StartupForOwin>(Config.Instance.Uri))
             {
-                Console.WriteLine("Opaitema LightWebserver on {0} is up.", uri);
+                Console.WriteLine("Opaitema LightWebserver on {0} is up.", Config.Instance.Uri);
                 Console.ReadKey();
                 
             }
 
-            Console.WriteLine("Opaitema LightWebserver on {0} is close.", uri);
+            Console.WriteLine("Opaitema LightWebserver on {0} is close.", Config.Instance.Uri);
             Console.WriteLine("Press any key for close.");
             Console.ReadKey();
+
+        }
+
+        static void ConfigInitWithParameters(string[] args) {
+            var folder = string.Empty;
+            int port = 9090;
+            var debugRequest = false;
+
+            //Argumentos
+            for (int i = 0; i < args.Length; i++)
+            {
+                var param = args[i].Trim().ToLower();
+
+                //Validate is directory exists
+                //Parameter: -f: folder
+                if (param.IndexOf("-f:") > -1 || param.IndexOf("/f:") > -1)
+                {
+                    folder = param.Substring(3, param.Length - 3);
+                    if (!System.IO.Directory.Exists(folder))
+                        throw new Exception(String.Format("The directory not exists {0}", folder));
+                }
+
+                //Parameter: -p: port
+                if (param.IndexOf("-p:") > -1 || param.IndexOf("/p:") > -1)
+                {
+                    var portCadena = param.Substring(3, param.Length - 3);
+                    if (!Int32.TryParse(portCadena, out port))
+                        throw new Exception("You must enter a number for the http port");
+                }
+
+                //Parameter: -d: debugRequest
+                if (param.IndexOf("-d:") > -1 || param.IndexOf("/d:") > -1)
+                {
+                    var debugRequestCadena = param.Substring(3, param.Length - 3);
+                    if (!Boolean.TryParse(debugRequestCadena, out debugRequest))
+                        throw new Exception("You must enter a Boolean value for debugRequest parameter");
+                }
+            }
+
+            Config.Instance.RootDirectory = String.IsNullOrEmpty(folder)
+                                                ? Environment.CurrentDirectory
+                                                : folder;
+
+            Config.Instance.Port = port;
+            Config.Instance.DebugRequest = debugRequest;
 
         }
     }
